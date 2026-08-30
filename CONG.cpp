7da7549d64aa04eb57c758b18bd60087ed5fabe5
xTony2k11 <xtony2k11@gmail.com>
2026-08-30 09:58:01 +0700

    Debug lỗi long long

--- CONG.cpp
+++ CONG.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 int main(){
-    int a, b;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    long long a, b;
     cin >> a >> b;
     cout << a + b;
     return 0;
